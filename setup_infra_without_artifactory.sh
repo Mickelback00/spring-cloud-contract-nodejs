8f3fe11312db6dd58d6f3e6ff6e7de57ad0dabac
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-07-05 19:48:19 +0200

    Version

--- setup_infra_without_artifactory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pushd docker
-docker-compose -f docker-compose-only-mongo.yml build
-docker-compose -f docker-compose-only-mongo.yml up -d
-popd
--- stop_infra.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pushd docker
-docker-compose kill || echo "Failed to kill docker"
-docker-compose -f docker-compose-only-mongo.yml kill || echo "Failed to kill docker"
-popd
